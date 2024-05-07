3fa66a4e4d0905b272dd6f6fa51ba78f3742bcf9
joaovic499 <joaovictorsilva60@live.com>
2024-05-07 14:13:43 -0300

    Adição de geração de tokens JWP no register e login de usuarios

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const UsuarioModel = require ("../models/Usuario");
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+require('dotenv').config()
+const secret = process.env.SECRET
+
+const generateToken = (Usuario = {}) => {
+    return jwt.sign({
+        id: Usuario.id,
+        name: Usuario.name
+    }, secret , {
+        expiresIn: 86400
+    });
+
+}
+
 
 router.post("/register", async(req, res) => {
     
@@ -20,10 +35,41 @@ router.post("/register", async(req, res) => {
 
     const Usuario = await UsuarioModel.create(req.body);
         Usuario.password = undefined;
-            return res.json({
-                message:"Registrado com sucesso",
-                    data: Usuario
+
+        return res.json({
+            Usuario,
+            token: generateToken(Usuario),
+            message:"Registrado com sucesso",
     });
 })
+
+router.post("/autenticate", async(req, res) => {
+    
+    const {email, password} = req.body;
+    const Usuario = await UsuarioModel.findOne({email}).select("+password");
+ 
+    if(!Usuario) {
+        return res.status(400).json({
+            message: "Usuario não encontrado"
+        })
+    }
+
+    if(!await bcrypt.compare(password, Usuario.password)){
+        return res.status(400).json({
+            message: "Senha incorreta!!!"
+        })
+
+    }
+
+    Usuario.password = undefined;
+    
+    return res.json({
+        Usuario,
+        token: generateToken(Usuario)
+    });
+    
+
+})
+
 
 module.exports = router;
