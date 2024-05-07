064b48a63fcf14aebce379dedd165f7ceb2e90ca
joaovic499 <joaovictorsilva60@live.com>
2024-05-07 11:52:59 -0300

    ADD o usuario para ele confirmar a senha na hora de cadastrar

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -3,12 +3,18 @@ const UsuarioModel = require ("../models/Usuario");
 const router = express.Router();
 
 router.post("/register", async(req, res) => {
-
-    const {email} = req.body;
+    
+    const {email, password, confirmpassword} = req.body;
 
     if(await UsuarioModel.findOne({email})){
         return res.status(400).json({
             message: "Email de usuario ja cadastrado, Cadastre um usuario com outro email!"
+        })
+    }
+
+    if (password != confirmpassword){
+        return res.status(400).json({
+            message: "Senhas não correspondem"
         })
     }
 
